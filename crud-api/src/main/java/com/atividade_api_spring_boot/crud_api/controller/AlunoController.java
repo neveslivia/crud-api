b828d66359191551184f373697ea6b135d446f0a
Lívia Neves Rodrigues Vitor Estevam <neveslivia95@gmail.com>
2025-05-27 15:21:11 -0300

    feat: Adicionei documentações básicas do Swagger

--- crud-api/src/main/java/com/atividade_api_spring_boot/crud_api/controller/AlunoController.java
+++ crud-api/src/main/java/com/atividade_api_spring_boot/crud_api/controller/AlunoController.java
@@ -3,19 +3,21 @@ package com.atividade_api_spring_boot.crud_api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.atividade_api_spring_boot.crud_api.model.Aluno;
 import com.atividade_api_spring_boot.crud_api.service.AlunoService;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 
 
@@ -29,12 +31,25 @@ public class AlunoController {
      public AlunoController(AlunoService alunoService) {
         this.alunoService = alunoService;
     }
+    
+    @Operation(
+        summary = "Buscar aluno por ID",
+        description = "Retorna os dados de um aluno específico com base no ID fornecido."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Aluno encontrado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Aluno não encontrado")
+    })
 
     @GetMapping("/{id}")
     public Aluno listarAlunos(@PathVariable Long id) {
         return alunoService.burcarAlunoPorId(id);
 
     }
+    @Operation(
+        summary = "Busca todos os alunos cadastrados",
+        description = "Retorna os dados de todos os alunos."
+    )
 
     @GetMapping
     public List<Aluno> listarAlunos() {
@@ -45,12 +60,30 @@ public class AlunoController {
     @PostMapping
     public Aluno cadastarAluno(@RequestBody Aluno aluno) {
         return alunoService.cadastrarAluno(aluno);
+        
     }
+    @Operation(
+        summary = "Deletar aluno por ID",
+        description = "Deleta os dados do aluno de acordo com o ID fornecido"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Aluno deletado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Aluno não encontrado")
+    })
 
     @DeleteMapping("/{id}")
     public void deletarAluno(@PathVariable Long  id){
         alunoService.deletarAluno(id);
     }
+
+    @Operation(
+        summary = "Atualiza  ID",
+        description = "Atualiza os dados de um aluno específico com base no ID fornecido."
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Aluno atualizado com sucesso"),
+        @ApiResponse(responseCode = "404", description = "Aluno não encontrado")
+    })
 
     @PutMapping("/{id}")
     public Aluno atualizarAluno(@PathVariable Long id, @RequestBody Aluno aluno) {
---
+++ crud-api/src/main/java/com/atividade_api_spring_boot/crud_api/controller/Operation.java
@@ -0,0 +1,9 @@
+package com.atividade_api_spring_boot.crud_api.controller;
+
+public @interface Operation {
+
+    String description();
+
+    String summary();
+
+}
